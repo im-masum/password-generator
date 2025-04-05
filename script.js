dd5704642fa6440d459f5ea55b26920bf1930f42
MIchowdhury <masum.iqbal.chowdhury@gmail.com>
2025-04-05 21:40:41 +0600

    modified

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let inputSlider = document.getElementById("input-slider");
 let inputValue = document.getElementById("input-value");
+let inputBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let button = document.getElementById("button");
+
+
+
 
 
 // Showing input slider value
@@ -9,3 +18,14 @@ sliderValue.textContent = inputSlider.value;
 
 });
 
+genBtn.addEventListener("click", ()=> {
+passBox.generatePassword();
+}); 
+
+// function to generate password
+function generatePassword(){
+    let genPassword = "";
+
+    genPassword = Math.floor( Math.random() * 100);
+    return genPassword
+}
